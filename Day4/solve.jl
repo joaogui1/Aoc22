56355a5604678e0babd95b4c6309cae93d194e5b
joaogui1 <joaoguilhermearujo@gmail.com>
2022-12-04 08:25:41 -0300

    golfing

--- Day4/solve.jl
+++ Day4/solve.jl
@@ -1,21 +1,14 @@
-# Part1
 to_int = x -> parse(Int, x)
 process(p) = map(to_int, split(p[1], '-')), map(to_int, split(p[2], '-'))
 inside(a, b) = (a[1] <= b[1]) && (a[2] >= b[2])
-function f(p)
-    rng1, rng2 = process(p)
-    inside(rng1, rng2) || inside(rng2, rng1)
+intersect_(rngs) = (rngs[1][2] >= rngs[2][1] >= rngs[1][1]) || (rngs[2][2] >= rngs[1][1] >= rngs[2][1])
+function part1(ranges)
+    ranges |> process |> p -> inside(p[1], p[2]) || inside(p[2], p[1])
+end
+function part2(ranges)
+    ranges |> process |> intersect_
 end
 open("input.txt", "r") do io
     assign = io |> readlines |> lines -> map(ln -> split(ln, ','), lines)
-    sum(map(f, assign))
-end
-# Part2
-function f(p)
-    rng1, rng2 = process(p)
-    (rng2[2] >= rng1[1] >= rng2[1]) || (rng1[2] >= rng2[1] >= rng1[1])
-end
-open("input.txt", "r") do io
-    assign = io |> readlines |> lines -> map(ln -> split(ln, ','), lines)
-    sum(map(f, assign))
+    sum(map(part1, assign)), sum(map(part2, assign))
 end
